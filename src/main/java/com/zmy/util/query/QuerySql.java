b6d8738e6d622e5c31835aba00afd1d853fa4f8d
Mingyu.Zhang <myzhang@sudytech.cn>
2021-12-16 18:06:44 +0800

    builder

--- src/main/java/com/zmy/util/query/QuerySql.java
+++ src/main/java/com/zmy/util/query/QuerySql.java
@@ -3,6 +3,7 @@ package com.zmy.util.query;
 import com.zmy.util.Sql;
 import com.zmy.util.query.component.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -27,7 +28,16 @@ public class QuerySql implements Sql {
 
     private List<OrderBy> orderBys;
 
-//    public QuerySql select(List<Select> selects) {
+    public QuerySql(List<Select> selects, From from, Where where, List<GroupBy> groupBys, Having having, List<OrderBy> orderBys) {
+        this.selects = selects;
+        this.from = from;
+        this.where = where;
+        this.groupBys = groupBys;
+        this.having = having;
+        this.orderBys = orderBys;
+    }
+
+    //    public QuerySql select(List<Select> selects) {
 //        this.selects = selects;
 //
 //    }
@@ -36,49 +46,49 @@ public class QuerySql implements Sql {
         return selects;
     }
 
-    public void setSelects(List<Select> selects) {
-        this.selects = selects;
-    }
+//    public void setSelects(List<Select> selects) {
+//        this.selects = selects;
+//    }
 
     public From getFrom() {
         return from;
     }
 
-    public void setFrom(From from) {
-        this.from = from;
-    }
+//    public void setFrom(From from) {
+//        this.from = from;
+//    }
 
     public Where getWhere() {
         return where;
     }
 
-    public void setWhere(Where where) {
-        this.where = where;
-    }
+//    public void setWhere(Where where) {
+//        this.where = where;
+//    }
 
     public List<GroupBy> getGroupBys() {
         return groupBys;
     }
 
-    public void setGroupBys(List<GroupBy> groupBys) {
-        this.groupBys = groupBys;
-    }
+//    public void setGroupBys(List<GroupBy> groupBys) {
+//        this.groupBys = groupBys;
+//    }
 
     public Having getHaving() {
         return having;
     }
 
-    public void setHaving(Having having) {
-        this.having = having;
-    }
+//    public void setHaving(Having having) {
+//        this.having = having;
+//    }
 
     public List<OrderBy> getOrderBys() {
         return orderBys;
     }
 
-    public void setOrderBys(List<OrderBy> orderBys) {
-        this.orderBys = orderBys;
-    }
+//    public void setOrderBys(List<OrderBy> orderBys) {
+//        this.orderBys = orderBys;
+//    }
 
     @Override
     public String toString() {
@@ -91,4 +101,59 @@ public class QuerySql implements Sql {
                 ", orderBys=" + orderBys +
                 '}';
     }
+
+    public static class QuerySqlBuilder {
+        private List<Select> selects = new ArrayList<>();
+
+        private From from;
+
+        private Where where;
+
+        private List<GroupBy> groupBys = new ArrayList<>();
+
+        private Having having;
+
+        private List<OrderBy> orderBys = new ArrayList<>();
+
+        public static QuerySqlBuilder builder() {
+            return new QuerySqlBuilder();
+        }
+
+        public QuerySqlBuilder selects(List<Select> selects) {
+            this.selects.addAll(selects);
+            return this;
+        }
+
+        public QuerySqlBuilder from(From from) {
+            this.from = from;
+            return this;
+        }
+
+        public QuerySqlBuilder where(Where where) {
+            this.where = where;
+            return this;
+        }
+
+        public QuerySqlBuilder groupBys(List<GroupBy> groupBys) {
+            this.groupBys.addAll(groupBys);
+            return this;
+        }
+
+        public QuerySqlBuilder having(Having having) {
+            this.having = having;
+            return this;
+        }
+
+        public QuerySqlBuilder orderBys(List<OrderBy> orderBys) {
+            this.orderBys.addAll(orderBys);
+            return this;
+        }
+
+        public QuerySql build() {
+            return new QuerySql(selects,from,where,groupBys,having,orderBys);
+        }
+
+    }
+
+
 }
--- src/test/java/QuerySqlTest.java
+++ src/test/java/QuerySqlTest.java
@@ -12,6 +12,7 @@ import org.junit.Test;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 
 /**
  * @program: sql-generate-util
@@ -23,54 +24,42 @@ public class QuerySqlTest {
 
     @Test
     public void test01() throws Exception {
-        QuerySql querySql = new QuerySql();
-        querySql.setSelects(
-                Arrays.asList(
-                        Select.SelectBuilder.builder().table("user").column("name").alias("用户名").build(),
-                        Select.SelectBuilder.builder().table("user").column("salary").alias("薪资").columnOper(ColumnOper.SUM).build(),
-                        Select.SelectBuilder.builder().table("department").column("name").alias("部门").build(),
-                        Select.SelectBuilder.builder().table("role").column("name").alias("角色").build()
-                )
+        List<Select> selects = Arrays.asList(
+                Select.SelectBuilder.builder().table("user").column("name").alias("用户名").build(),
+                Select.SelectBuilder.builder().table("user").column("salary").alias("薪资").columnOper(ColumnOper.SUM).build(),
+                Select.SelectBuilder.builder().table("department").column("name").alias("部门").build(),
+                Select.SelectBuilder.builder().table("role").column("name").alias("角色").build()
         );
         //构造from
-        querySql.setFrom(
-                From.FromBuilder.builder().table("user").tableAlias("u").build().leftJoin(
-                        From.FromBuilder.builder().table("department").tableAlias("d1").build().on(
-                                On.OnBuilder.builder().table1("u").column1("id").condOper(CondOper.EQUAL).table2("d1").column2("user_id").build()
-                                        .or(On.OnBuilder.builder().table1("u").column1("name").condOper(CondOper.EQUAL).table2("d1").column2("user_name").build())
-                                        .andPart(On.OnBuilder.builder().table1("u").column1("col1").condOper(CondOper.EQUAL).table2("d1").column2("col2").build())
-                        )
-                ).leftJoin(From.FromBuilder.builder().table("role").tableAlias("r1").build().on(On.OnBuilder.builder().table1("u").column1("id").condOper(CondOper.EQUAL).table2("r1").column2("user_id").build()))
-        );
-        //构造where
-        querySql.setWhere(
-                Where.WhereBuilder.builder().table("user").column("id").condOper(CondOper.EQUAL).value(123456789).build()
-                .or(Where.WhereBuilder.builder().table("department").column("id").condOper(CondOper.EQUAL).value("de456852").build())
-                .partAnd(Where.WhereBuilder.builder().table("user").column("create_time").condOper(CondOper.GREATERTHANOREQUAL).value(new Date()).build())
-       );
-        //构造group by
-        querySql.setGroupBys(
-                Arrays.asList(
-                        GroupBy.GroupByBuilder.builder().table("user").column("id").build(),
-                        GroupBy.GroupByBuilder.builder().table("role").column("id").build()
+        From from = From.FromBuilder.builder().table("user").tableAlias("u").build().leftJoin(
+                From.FromBuilder.builder().table("department").tableAlias("d1").build().on(
+                        On.OnBuilder.builder().table1("u").column1("id").condOper(CondOper.EQUAL).table2("d1").column2("user_id").build()
+                                .or(On.OnBuilder.builder().table1("u").column1("name").condOper(CondOper.EQUAL).table2("d1").column2("user_name").build())
+                                .andPart(On.OnBuilder.builder().table1("u").column1("col1").condOper(CondOper.EQUAL).table2("d1").column2("col2").build())
                 )
+        ).leftJoin(From.FromBuilder.builder().table("role").tableAlias("r1").build().on(On.OnBuilder.builder().table1("u").column1("id").condOper(CondOper.EQUAL).table2("r1").column2("user_id").build()));
+        //构造where
+        Where where = Where.WhereBuilder.builder().table("user").column("id").condOper(CondOper.EQUAL).value(123456789).build()
+                .or(Where.WhereBuilder.builder().table("department").column("id").condOper(CondOper.EQUAL).value("de456852").build())
+                .partAnd(Where.WhereBuilder.builder().table("user").column("create_time").condOper(CondOper.GREATERTHANOREQUAL).value(new Date()).build());
+        //构造group by
+
+        List<GroupBy> groupBys = Arrays.asList(
+                GroupBy.GroupByBuilder.builder().table("user").column("id").build(),
+                GroupBy.GroupByBuilder.builder().table("role").column("id").build()
         );
         //构造having
-        querySql.setHaving(
-                Having.HavingBuilder.builder().table("user").column("age").condOper(CondOper.GREATERTHAN).value(20).build()
+        Having having = Having.HavingBuilder.builder().table("user").column("age").condOper(CondOper.GREATERTHAN).value(20).build()
                 .and(Having.HavingBuilder.builder().table("user").column("gender").condOper(CondOper.EQUAL).value("女").build())
-                .partOr(Having.HavingBuilder.builder().table("user").column("gg").condOper(CondOper.ISNULL).build())
-
-        );
+                .partOr(Having.HavingBuilder.builder().table("user").column("gg").condOper(CondOper.ISNULL).build());
         //构造order by
-        querySql.setOrderBys(
-                Arrays.asList(
-                        OrderBy.OrderByBuilder.builder().table("user").column("id").order(Order.ASC).build(),
-                        OrderBy.OrderByBuilder.builder().table("department").column("name").order(Order.ASC).build(),
-                        OrderBy.OrderByBuilder.builder().table("role").column("id").order(Order.DESC).build()
-                )
+        List<OrderBy> orderBys = Arrays.asList(
+                OrderBy.OrderByBuilder.builder().table("user").column("id").order(Order.ASC).build(),
+                OrderBy.OrderByBuilder.builder().table("department").column("name").order(Order.ASC).build(),
+                OrderBy.OrderByBuilder.builder().table("role").column("id").order(Order.DESC).build()
         );
-        System.out.println(QuerySqlParser.builder().parse(querySql));
+        QuerySql build = QuerySql.QuerySqlBuilder.builder().selects(selects).from(from).where(where).groupBys(groupBys).having(having).orderBys(orderBys).build();
+        System.out.println(QuerySqlParser.builder().parse(build));
     }
 
     @org.junit.Test
@@ -83,10 +72,9 @@ public class QuerySqlTest {
         map.put("3", Where.WhereBuilder.builder().table("3").column("3").condOper(CondOper.EQUAL).value("3").build());
         map.put("4", Where.WhereBuilder.builder().table("4").column("4").condOper(CondOper.EQUAL).value("4").build());
         Where where = RuleUtil.handler(ruleNode, map);
-        QuerySql querySql = new QuerySql();
-        querySql.setFrom(From.FromBuilder.builder().table("s").build());
-        querySql.setWhere(where);
-        System.out.println(QuerySqlParser.builder().parse(querySql));
+        From from = From.FromBuilder.builder().table("s").build();
+        QuerySql build = QuerySql.QuerySqlBuilder.builder().from(from).where(where).build();
+        System.out.println(QuerySqlParser.builder().parse(build));
 
     }
 
@@ -95,10 +83,9 @@ public class QuerySqlTest {
         Where where = Where.WhereBuilder.builder().table("user").column("id").condOper(CondOper.EQUAL).value(123456789).build()
                 .or(Where.WhereBuilder.builder().table("department").column("id").condOper(CondOper.EQUAL).value("de456852").build())
                 .partAnd(Where.WhereBuilder.builder().table("user").column("create_time").columnOper(ColumnOper.DATE_FORMAT).condOper(CondOper.GREATERTHANOREQUAL).value(new DateTime()).build());
-        QuerySql querySql = new QuerySql();
-        querySql.setFrom(From.FromBuilder.builder().table("s").build());
-        querySql.setWhere(where);
-        System.out.println(QuerySqlParser.builder().parse(querySql));
+        From from = From.FromBuilder.builder().table("s").build();
+        QuerySql build = QuerySql.QuerySqlBuilder.builder().from(from).where(where).build();
+        System.out.println(QuerySqlParser.builder().parse(build));
     }
 
     @org.junit.Test
@@ -106,10 +93,9 @@ public class QuerySqlTest {
         Where where = Where.WhereBuilder.builder().table("user").column("id").condOper(CondOper.EQUAL).value(123456789).build()
                 .or(Where.WhereBuilder.builder().table("department").column("id").condOper(CondOper.EQUAL).value("de456852").build())
                 .partAnd(Where.WhereBuilder.builder().table("user").column("create_time").condOper(CondOper.GREATERTHANOREQUAL).value(new DateTime()).build());
-        QuerySql querySql = new QuerySql();
-        querySql.setFrom(From.FromBuilder.builder().table("s").build());
-        querySql.setWhere(where);
-        System.out.println(QuerySqlParser.builder().parse(querySql));
+        From from = From.FromBuilder.builder().table("s").build();
+        QuerySql build = QuerySql.QuerySqlBuilder.builder().from(from).where(where).build();
+        System.out.println(QuerySqlParser.builder().parse(build));
     }
 
     @org.junit.Test
@@ -117,10 +103,9 @@ public class QuerySqlTest {
         Where where = Where.WhereBuilder.builder().table("user").column("id").condOper(CondOper.EQUAL).value(123456789).build()
                 .or(Where.WhereBuilder.builder().table("department").column("id").condOper(CondOper.EQUAL).value("de456852").build())
                 .partAnd(Where.WhereBuilder.builder().table("user").column("create_time").condOper(CondOper.ISNULL).build());
-        QuerySql querySql = new QuerySql();
-        querySql.setFrom(From.FromBuilder.builder().table("s").build());
-        querySql.setWhere(where);
-        System.out.println(QuerySqlParser.builder().parse(querySql));
+        From from = From.FromBuilder.builder().table("s").build();
+        QuerySql build = QuerySql.QuerySqlBuilder.builder().from(from).where(where).build();
+        System.out.println(QuerySqlParser.builder().parse(build));
     }
 
 }
